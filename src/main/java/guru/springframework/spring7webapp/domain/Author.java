e6aa428bf5eae281568b14817407e36491b49874
GabrielBlanc0o <gtbvacation@outlook.es>
2026-07-17 16:12:31 -0500

    feat: discovering intellij IDE :D

---
+++ src/main/java/guru/springframework/spring7webapp/domain/Author.java
@@ -0,0 +1,41 @@
+package guru.springframework.spring7webapp.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String primerNombre;
+    private String apellido;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPrimerNombre() {
+        return primerNombre;
+    }
+
+    public void setPrimerNombre(String primerNombre) {
+        this.primerNombre = primerNombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+}
